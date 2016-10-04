a6b988df5bf8acb8e6eef084312f4ef3995259b8
wboo7 <326251409@qq.com>
2016-10-04 09:22:09 +0800

    modify ignore for main-local

---
+++ frontend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'kcaGqsvP353ANWWxtQ6xfwllgGI7CQxY',
+        ],
+
+    ],
+];
+
+if (!YII_ENV_TEST) {
+
+   //  configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'defaultRoute'=>'default/index'
+    ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
