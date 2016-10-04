c24887e968abf5e3d6c07d2124454404e97692bb
wboo7 <326251409@qq.com>
2016-10-04 09:15:47 +0800

    local config and params

---
+++ admin/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '_tr5RX1ZXG6BX8cVv0yD_1dJ94h_5Fkd',
+        ],
+
+
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+//    $config['bootstrap'][] = 'debug';
+//    $config['modules']['debug'] = [
+//        'class' => 'yii\debug\Module',
+//    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
